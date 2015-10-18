0e1602b80f86ba7e788baac9d28d6a36e67b9c25
chrisTi <spongebernd@gmx.net>
2015-10-17 22:16:01 -0700

    added LGS_intersect_lines.m

---
+++ TIOPTI/LGS_intersect_lines.m
@@ -0,0 +1,21 @@
+A = [ 
+    5 2
+    .2 .4
+    .2 .2
+    ];
+b = [ 200 ; 16 ; 10 ];
+c = [20 ; 26];
+
+pick = [1 2] % select inqeuality pairs for lines here
+
+intersection_x = A(pick,:)\b(pick,:);
+
+fx = c'*intersection_x
+
+satisfied_inequalities = A*intersection_x <= b;
+if (all(satisfied_inequalities))
+    disp('-> feasible solution')
+else
+    disp('-> NOT IN THE FEASIBLE SET!')
+    satisfied_inequalities
+end
