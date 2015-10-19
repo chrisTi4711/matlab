f20fc5c9a35e54dbe4ddc1e04d083f2ab1b46a64
chrisTi <spongebernd@gmx.net>
2015-10-19 01:11:03 -0700

    added FibonacciSearch.m; reformatted GoldenSectionSearch.m

--- TIOPTI/FibonacciSearch.m
+++ TIOPTI/FibonacciSearch.m
@@ -5,20 +5,34 @@ f = @(x) 8.*exp(1-x)+7.*log(x);
 left = 1;
 right = 2;
 uncert = 0.23;
+epsilon = 0.05;
 
-rho = (3-sqrt(5))/2;
+F(1)=1;
+F(2)=1;
 
-N = ceil(log(uncert/(right-left))/log(1-rho))
+N=0;
+while F(N+2) < (1+2*epsilon)*(right-left)/uncert
+    F(N+3)=F(N+2)+F(N+1);
+    N=N+1;
+end
+
+N %print N
 
 lower='a';
-a=left+(1-rho)*(right-left);
+a=left+(F(N+1)/F(N+2))*(right-left);
 f_a=f(a);
 
 fprintf('-------------------------------------------------------------------------------------\n');
-fprintf('k     a           b           f(a)        f(b)   lower    New uncertainity interval  \n');
+fprintf('k    rho         a           b           f(a)        f(b)   lower    New uncertainity interval  \n');
 fprintf('-------------------------------------------------------------------------------------\n');
 
 for i=1:N
+    if i~=N
+        rho=1-F(N+2-i)/F(N+3-i);
+    else
+        rho=0.5-epsilon;
+    end
+    
     if lower=='a'
         b=a;
         f_b=f_a;
@@ -41,7 +55,7 @@ for i=1:N
     
     New_Interval = [left,right];
     
-    fprintf('%i: %f    %f    %f    %f    %c        [%f %f] \n', i, a, b, f_a, f_b, lower, left, right);
+    fprintf('%i: %f   %f    %f    %f    %f    %c        [%f %f] \n', i, rho, a, b, f_a, f_b, lower, left, right);
 end
 
 fprintf('\nfinal accuracy after %i iterations:\n   %f < %f\n', N, right - left, uncert);
