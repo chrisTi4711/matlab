c158b24d6c81a8524fd18f186cf68cee630ab59d
chrisTi <spongebernd@gmx.net>
2015-10-20 06:53:17 -0700

    fixed iteration output

--- TIOPTI/ConjugateGradientAlgorithm.m
+++ TIOPTI/ConjugateGradientAlgorithm.m
@@ -1,19 +1,20 @@
 clear;clc;
 
-Q = [2 -2;-2 6];
-b = [0;-3];
-x = [1;1]; % starting point
+Q = [5 2;2 1];
+b = [3;1];
+x = [0;0]; % starting point
 
 %2
 g = Q*x-b;
 fprintf('g(0) = \n');disp(g);
 
 if all(g == 0); return; end
+
+fprintf('Iteration %i: ---------------------\n',1);
 d = -g;
-fprintf('d(0) = \n');disp(d);
+fprintf('d(0) = -g(0) = \n');disp(d);
 
 for k=0:3
-    fprintf('Iteration %i: ---------------------\n',k+1);
     %3
     alpha = -(g'*d)/(d'*Q*d);
     fprintf('alpha_%i = \n',k);disp(alpha);
@@ -23,11 +24,12 @@ for k=0:3
     fprintf('x(%i) = \n',k+1);disp(xx);
     
     %5
-    gg = Q*xx-b;
-    fprintf('g(%i) = \n',k+1);disp(gg);
+    gg = Q*xx-b;    
     if all(gg == 0); return; end
+    fprintf('g(%i) = \n',k+1);disp(gg);
     
     %6
+    fprintf('Iteration %i: ---------------------\n',k+2);
     beta = (gg'*Q*d)/(d'*Q*d);
     fprintf('beta_%i = \n',k);disp(beta);
     
