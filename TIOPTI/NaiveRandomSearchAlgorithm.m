ef08274a78d9625ad5ab15fba29ecb7e39306aeb
chrisTi <spongebernd@gmx.net>
2015-10-21 02:08:58 -0700

    improved algorithm -> now possible to use m>1 vectors

--- TIOPTI/NaiveRandomSearchAlgorithm.m
+++ TIOPTI/NaiveRandomSearchAlgorithm.m
@@ -3,12 +3,11 @@ clear; clc;
 f = @(x) x.^4-8*x.^2+x; % W shape; left is global minimum, right is local minumum
 
 x = 3;
-N = 1; % width of neighborhood
+alpha = 0.5; % width of neighborhood (zk - alpha <= xk <= zk + alpha)
 
 for k=0:1000
-    a = x-0.5*N;
-    b = x+0.5*N;
-    z = (b-a).*rand(1,1) + a;
+    d = 2.*alpha.*rand(1,1)-alpha;
+    z = x+d;
     
     if f(z) < f(x)
         x = z;
--- TIOPTI/SimulatedAnnealingAlgorithm_own.m
+++ TIOPTI/SimulatedAnnealingAlgorithm_own.m
@@ -3,14 +3,13 @@ clear; clc;
 f = @(x) x.^4-8*x.^2+x; % W shape; left is global minimum, right is local minumum
 
 x = 3;
-N = 1; % width of neighborhood
+alpha = 0.5; % width of neighborhood (zk - alpha <= xk <= zk + alpha)
 gamma = 100;
 xbest = x;
 
 for k=0:1000
-    a = x-0.5*N;
-    b = x+0.5*N;
-    z = (b-a).*rand(1,1) + a;
+    d = 2.*alpha.*rand(1,1)-alpha;
+    z = x+d;
 
     cointoss = rand(1);
     T = gamma/log(k+2);
