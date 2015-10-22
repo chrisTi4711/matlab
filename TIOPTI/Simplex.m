628576bcf22a8de5a4fe9e5a1cf23227c3b3ccf9
chrisTi <spongebernd@gmx.net>
2015-10-22 07:22:23 -0700

    moved minus from c

--- TIOPTI/Simplex.m
+++ TIOPTI/Simplex.m
@@ -7,8 +7,8 @@ A = [
     3 1 5
     1 2 1
     ];
-b =  [60; 46; 50];
-c = -[25; 33; 18];
+b = [60; 46; 50];
+c = [25; 33; 18];
 
 % auto add slack variables:
 n_slack = size(b,1);
@@ -19,5 +19,5 @@ c = [c;zeros(size(b,1),1)]
 v = (size(A,2)-n_slack+1):size(A,2) %slack var columns
 
 %option five to show details, like pivot element, and intermediate passages
-simplex(c,A,b,v,5);
+simplex(-c,A,b,v,5);
 
