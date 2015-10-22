3c36d1fef445feb84f1b0688084203c7f6a5a9ba
chrisTi <spongebernd@gmx.net>
2015-10-22 07:21:47 -0700

    added auto add of slack vars

--- TIOPTI/Simplex.m
+++ TIOPTI/Simplex.m
@@ -1,17 +1,22 @@
-clc
+clear;clc
 % (Values from Lay Exercise 5)
 
-% A contains the constraint AND the slack variables
+% A contains the constraint
 A = [
-    2 3 4 1 0 0
-    3 1 5 0 1 0 
-    1 2 1 0 0 1
+    2 3 4
+    3 1 5
+    1 2 1
     ];
 b =  [60; 46; 50];
-c = -[25; 33; 18; 0; 0; 0];
+c = -[25; 33; 18];
 
-%specify here the slack variables columns 
-v = [4;5;6];
+% auto add slack variables:
+n_slack = size(b,1);
+A = [A,eye(n_slack)]
+b
+c = [c;zeros(size(b,1),1)]
+
+v = (size(A,2)-n_slack+1):size(A,2) %slack var columns
 
 %option five to show details, like pivot element, and intermediate passages
 simplex(c,A,b,v,5);
